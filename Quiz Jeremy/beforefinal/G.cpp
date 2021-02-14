df8f1c4f56c5795dd57d831e2d0230e92ef9223a
JoshuaTimothyW <joshuatimothywongso@gmail.com>
2021-02-14 19:30:39 +0700

    add G solution

--- Quiz Jeremy/beforefinal/G.cpp
+++ Quiz Jeremy/beforefinal/G.cpp
@@ -1,32 +1,32 @@
 #include <stdio.h>
+#define ll long long int
 
 struct Divisor{
-	int ori;
-	int sum;
+	ll ori;
+	ll sum;
 }arr[1001];
 
-int divisor(int n){
+ll divisor(ll n){
 	
-	int res=0; 
-  
-    for (int i=2; i*i<=n; i++) 
-    { 
-        if( n%i == 0 ) 
+	ll result = 1;
+	
+	if( n == 1 ){
+		return result;
+	}
+	
+	for(int i=2;i*i <= n;i++){
+		if( n%i == 0 ) 
         { 
             if( i == (n/i) ){
-            	res += i; 	
+            	result += i; 	
 			} 
             else{
-            	res += (i + n/i);	
+            	result += (i + n/i);	
 			} 
         } 
-    } 
-  	
-  	if(n > 1){
-  		return (res+n+1);	
 	}
-	  	
-    return res+1; 
+	
+	return result+n;
 }
 
 void swap(Divisor *x,Divisor *y){ 
@@ -35,52 +35,42 @@ void swap(Divisor *x,Divisor *y){
 	*y = temp; 
 }
 
-void sort(int start,int end) {
-	int i,j;
-	
-	if(start < end){
-		i = start;  j = end+1;
-		
-		do{
-			do{ 
-				i=i+1;
-			} while(arr[i].sum < arr[start].sum);
-			
-			do{
-				j=j-1;
-			} while(arr[j].sum > arr[start].sum);
-			
-			if(i < j){
-				swap(&arr[i],&arr[j]);
-			}
-			
-		}while(i <= j);
-	
-	    swap(&arr[start],&arr[j]);
-	    sort(start,j-1);
-	    sort(j+1,end);
+int check(int index1, int index2){
+	if( arr[index1].sum == arr[index2].sum ){
+		 return arr[index1].ori > arr[index2].ori;
 	}
+	
+	 return arr[index1].sum > arr[index2].sum;
+}
+
+void sort(int n){ 
+
+	for(int i=0;i<(n-1);i++){
+		for(int j=0;j<n-(i+1);j++){
+			if( check(j,j+1) ){
+				swap(&arr[j],&arr[j+1]);
+			}
+		} 
+	} 
 }
 
 int main(){
 	
-	int n;
+	ll n;
 	
-	scanf("%d",&n);
+	scanf("%lld",&n);
 	
 	for(int i=0;i<n;i++){
-		scanf("%d",&arr[i].ori);
-		arr[i].sum = divisor(arr[i].ori);	
+		scanf("%lld",&arr[i].ori);
+		arr[i].sum = divisor(arr[i].ori);
 	}
 	
-	sort(0,n-1);
+	sort(n);
 	
-	printf("%d",arr[0].ori);
-	for(int i=1;i<n;i++){
-		printf(" %d",arr[i].ori);
+	for(int i=0;i<n-1;i++){
+		printf("%lld ",arr[i].ori);	
 	}
-	printf("\n");
+	printf("%lld\n",arr[n-1].ori);
 	
-    return 0;
+	return 0;
 }
-    
--- Quiz Jeremy/beforefinal/README.md
+++ Quiz Jeremy/beforefinal/README.md
@@ -5,7 +5,10 @@
 - Case C : "Domain Filtering"
 - Case D : "Find the Word"
 - Case E : "Company Dinner"
+- Case F : "Party Beverages"
+- Case G : "Divisor Sort"
 - Case H : "Bubble Shuffle"
+- Case L : "The Famous Gauss"
 
 # Notes
 
@@ -24,5 +27,17 @@ Find the matched first letter in puzzle, check each letter at horizontal and ver
 - Case E : 
 Same as case C, find the first and last index, get the result by: [last index]-[first index]+1.
 
+- Case F :
+Give additional conditions in binary search, like so:
+    - If middle array + number <= total and mid < n-1 then check if mid+1 array > total then return mid index else set start = mid+1
+    - else set end = mid-1
+If the result is same as number or -1 then do not compare with max value.
+
+- Case G : 
+A very simple way is to make an array of struct with 2 attributes: original number and sum of its divisor, then follow the instruction on how to sort the array.
+
 - Case H :
-create 3 array : array A (ori), array B (permute), and list_index. compare each number from array A with array B, if the numbers are equal put the array A index in list_index, sort array list_index using bubble sort and count every swap in bubble sort logic. 
+Create 3 array : array A (ori), array B (permute), and list_index. compare each number from array A with array B, if the numbers are equal put the array A index in list_index, sort array list_index using bubble sort and count every swap in bubble sort logic. 
+
+- Case L :
+Use 2 binary search to find minimum and maximum value in array. If the first number in array is larger than maximum number in range or the last number in array is smaller than  minimum number in range return -1. Otherwise sum numbers from the instrution. 
